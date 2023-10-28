4939b72a8bb380e2bb2dd1a75dddb6bd81f77c7e
QúyQúy <125336352+QuyQuy123@users.noreply.github.com>
2023-10-28 18:14:34 +0700

    Update checkso2cs.c

--- checkso2cs.c
+++ checkso2cs.c
@@ -8,16 +8,35 @@ int main() {
 
 	int a[100];
 	int i;
-	for(i=0;i<7;i++){
+	for(i=0; i<7; i++) {
 		scanf("\n%d",&a[i]);
 	}
-	int index =1;
-	
+	int index=0,j;
+
+
 // Fixed Do not edit anything here.
 	printf("\nOUTPUT: \n");
 // @STUDENT : WRITE YOUR OUTPUT HERE:
 
+	for(i=0; i<7; i++) {
+		if(a[i] >9 && a[i] <100) {
+			index =1;
+			int var_index =0;
+			for(j=0; j<i; j++) {
+				if(a[i] == a[j]) {
+					var_index=1;
+					break;
 
+				}
+			}
+			if(var_index != 1){
+				printf("%d ",a[i]);
+			}
+		}
+	}
+	if(index ==0) {
+		printf("No two-digit number ");
+	}
 	printf("\n");
 	system("pause");
 	return 0;
