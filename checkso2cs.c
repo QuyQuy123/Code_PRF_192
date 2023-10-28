0eea6215b01ba315bc2991f90e4367383afb11d4
QUYQUY123 <quyhslc11@gmail.com>
2023-10-28 17:41:51 +0700

    first commit

---
+++ checkso2cs.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+int main() {
+	system("cls");
+// INPUT - @STUDENT: ADD YOUR CODE FOR INPUT HERE:
+
+	int a[100];
+	int i;
+	for(i=0;i<7;i++){
+		scanf("\n%d",&a[i]);
+	}
+	int index =1;
+	
+// Fixed Do not edit anything here.
+	printf("\nOUTPUT: \n");
+// @STUDENT : WRITE YOUR OUTPUT HERE:
+
+
+	printf("\n");
+	system("pause");
+	return 0;
+}
